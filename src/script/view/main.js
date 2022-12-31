f7c41d8417d386597f1779503deec6bee09a9315
rifqiahmadf <rahmadfauzi600@gmail.com>
2022-12-31 23:14:02 +0700

    change to arrow function

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,36 +1,43 @@
-var main = function () {
-  var searchElement = document.querySelector('#searchElement');
-  var buttonSearchElement = document.querySelector('#searchButtonElement');
-  var clubListElement = document.querySelector('#clubList');
+var main = () => {
+  var searchElement = document.querySelector("#searchElement");
+  var buttonSearchElement = document.querySelector("#searchButtonElement");
+  var clubListElement = document.querySelector("#clubList");
 
-  var onButtonSearchClicked = function () {
+  var onButtonSearchClicked = () => {
     var dataSource = new DataSource(renderResult, fallbackResult);
     dataSource.searchClub(searchElement.value);
   };
 
-  var renderResult = function (results) {
-    clubListElement.innerHTML = '';
-    results.forEach(function (club) {
+  var renderResult = (results) => {
+    clubListElement.innerHTML = "";
+    results.forEach((club) => {
       var name = club.name;
       var fanArt = club.fanArt;
       var description = club.description;
 
-      var clubElement = document.createElement('div');
-      clubElement.setAttribute('class', 'club');
+      var clubElement = document.createElement("div");
+      clubElement.setAttribute("class", "club");
 
-      clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-          '<div class="club-info">\n' +
-          '<h2>' + name + '</h2>\n' +
-          '<p>' + description + '</p>' +
-          '</div>';
+      clubElement.innerHTML =
+        '<img class="fan-art-club" src="' +
+        fanArt +
+        '" alt="Fan Art">\n' +
+        '<div class="club-info">\n' +
+        "<h2>" +
+        name +
+        "</h2>\n" +
+        "<p>" +
+        description +
+        "</p>" +
+        "</div>";
       clubListElement.appendChild(clubElement);
     });
   };
 
-  var fallbackResult = function (message) {
-    clubListElement.innerHTML = '';
-    clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>';
+  var fallbackResult = (message) => {
+    clubListElement.innerHTML = "";
+    clubListElement.innerHTML += '<h2 class="placeholder">' + message + "</h2>";
   };
 
-  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
+  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
 };
